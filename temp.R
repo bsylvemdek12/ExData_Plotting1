411c484807133baa61f7e7bd0da0d0d6ecd290c3
bsylvemdek12 <bsylve@mdek12.org>
2016-11-09 07:34:14 -0600

    Delete temp.R

--- temp.R
+++
@@ -1,8 +0,0 @@
-printPlot1 <- function()
-{
-  colors = c("red")
-  powerdata <- read.csv("household_power_consumption.txt", sep=";")
-  powerdata$Date = as.Date(powerdata$Date, "%d/%m/%Y")
-  powerdatatwodays <- powerdata[powerdata$Date >= startDate & powerdata$Date <= endDate,]
-  hist(as.numeric(powerdatatwodays$Global_active_power),col=colors,xlab="Global Active Power (kilowatts)",main="Global Active Power")
-}
